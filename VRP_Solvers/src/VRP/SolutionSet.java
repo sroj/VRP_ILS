17eef9d505d4374f3f6f1aa16a389303875be9f5
volkoshkursk <30547621+volkoshkursk@users.noreply.github.com>
2019-03-16 19:58:59 +0300

    Update SolutionSet.java

--- VRP_Solvers/src/VRP/SolutionSet.java
+++ VRP_Solvers/src/VRP/SolutionSet.java
@@ -8,6 +8,8 @@ public abstract class SolutionSet {
 
     //DERIVAR ESTA CLASE CON LOS ATRIBUTOS PARTICULARES QUE RESULTEN
     //INTERESANTES PARA CADA ALGORITMO, Y SUS GETTERS Y SETTERS.
+    // ПОЛУЧИТЬ ЭТОТ КЛАСС С КОНКРЕТНЫМИ АТРИБУТАМИ, КОТОРЫЕ ВОЗНИКАЮТ
+    // ИНТЕРЕСНЫЕ ДЛЯ КАЖДОГО АЛГОРИТМА, А ТАКЖЕ ИХ ГЕТТЕРЫ И СЕТТЕРЫ.
     protected double executionTime;
 
     protected SolutionSet(double executionTime) {
